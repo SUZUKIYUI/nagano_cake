9bde081f8eaeaf78281c8d3af4b56f25308a1ab5
EC2 Default User <ec2-user@ip-172-31-70-240.ec2.internal>
2021-03-17 09:54:53 +0000

    [Add]enum(ja.ymlファイル作成/orderモデルのmethod_of_paymentとorder_statusの値をenumで設定)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+  enum method_of_payment:{
+    credit:0,
+    bank:1,
+  }
+  enum order_status:{
+    waiting_for_payment:0,
+    payment_confirmation:1,
+    in_production:2,
+    preparing_to_ship:3,
+    shipped:4,
+  }
 end
