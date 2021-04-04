53d1b81db90f3ca02f3f5ffa682306ee18cd94ce
EC2 Default User <ec2-user@ip-172-31-70-240.ec2.internal>
2021-04-04 01:57:28 +0000

    [Fix]ordersコントローラ/chekアクション　[Fix]注文情報入力画面/お届け先　修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,23 +6,28 @@ class Public::OrdersController < ApplicationController
 
   def check
     @order = Order.new
+    if params[:order][:method_of_payment] == "credit"
+      @order.method_of_payment = 0
+    elsif params[:order][:method_of_payment] == "bank"
+      @order.method_of_payment = 1
+    end
+
     if params[:order][:address_select] == "my_address"
+       # 『ご自身の住所』が選択された場合
       @order.delivery_address = current_customer.address
       @order.delivery_postal_code = current_customer.postal_code
       @order.delivery_addressee = current_customer.last_name + current_customer.first_name
     elsif params[:order][:address_select] == "other_address"
-      # address.find
-      @address = Address.find(params[:address_select])
+      # 『登録済住所から選択』が選択された場合
+      @address = Address.find(params[:order][:address_id])
       @order.delivery_address = @address.address
       @order.delivery_postal_code = @address.postal_code
       @order.delivery_addressee = @address.name
     elsif params[:order][:address_select] == "new_address"
-      # address.save
+      # 『新しいお届け先』が選択された場合
       @order = Order.new(order_params)
-      if @order.save == false
-        render :new
-      end
     end
+
   end
 
   def thanks
