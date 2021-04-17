d049f1a54464f43afbeab7d758266580f2efd69e
EC2 Default User <ec2-user@ip-172-31-70-240.ec2.internal>
2021-04-17 21:19:27 +0000

    [Add]customerコントローラ・モデルファイル/updateアクション・current_customer_params・バリデーション設定

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,10 +4,14 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-    
   end
 
   def update
+    if current_customer.update(current_customer_params)
+      redirect_to current_customer_path
+    else
+      render :edit
+    end
   end
 
   def leave
@@ -18,6 +22,13 @@ class Public::CustomersController < ApplicationController
     if current_customer.update
       redirect_to root_path
     end
+  end
+
+  # この下に何も記述しないこと！
+  private
+
+  def current_customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 
 end
