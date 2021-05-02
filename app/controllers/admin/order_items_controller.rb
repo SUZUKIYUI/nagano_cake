698dd05332420b9b230e690bb92508426d893734
EC2 Default User <ec2-user@ip-172-31-70-240.ec2.internal>
2021-05-02 10:19:52 +0000

    [Add/Fix]管理者order_itemsコントローラ・注文履歴詳細画面/updateアクション記述・制作ステータス（f.select）記述修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,6 +2,17 @@ class Admin::OrderItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
+    @order_item = OrderItem.find(params[:id])
+
+    if @order_item.update(order_item_params)
+      redirect_to admin_order_path(@order_item.order_id)
+    end
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:production_status)
   end
 
 end
