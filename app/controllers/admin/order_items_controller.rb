a23c83e69785b4f06a7a12fae9d086936a42e003
EC2 Default User <ec2-user@ip-172-31-70-240.ec2.internal>
2021-05-04 08:21:54 +0000

    [Fix]管理者order_itemsコントローラ/updateアクションに注文ステータスの自動更新機能設定

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,12 +3,21 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     @order_item = OrderItem.find(params[:id])
+    @order_item.update(order_item_params)
+    @order = Order.find(@order_item.order_id)
+    @order_items = OrderItem.where(order_id: @order.id)
 
-    if @order_item.update(order_item_params)
+    if @order_item.production_status == "in_production"
+      @order.order_status = "in_production"
+      @order.save
+      redirect_to admin_order_path(@order_item.order_id)
+    elsif @order_items.count == @order_item.order.order_items.where(production_status: "production_completed").count
+      @order.update(order_status: "preparing_to_ship")
       redirect_to admin_order_path(@order_item.order_id)
     end
   end
 
+# この下に何も記述しないこと！
   private
 
   def order_item_params
