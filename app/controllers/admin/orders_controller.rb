fb1712b51cf1a0677adbdc6e5edb57f30c11504a
EC2 Default User <ec2-user@ip-172-31-70-240.ec2.internal>
2021-05-04 07:08:21 +0000

    [Fix]管理者ordersコントローラ/updateアクションに製作ステータスの自動更新機能設定

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,8 +7,21 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order=Order.find(params[:id])
-
-    if @order.update(order_params)
+    @order_items=OrderItem.where(order_id: @order.id)
+    @order.update(order_params)
+    if @order.order_status == "waiting_for_payment"
+      @order_items.each do |order_item|
+        order_item.production_status = "not_startable"
+        order_item.save
+      end
+      redirect_to admin_order_path(@order.id)
+    elsif @order.order_status == "payment_confirmation"
+      @order_items.each do |order_item|
+        order_item.production_status = "waiting_for_production"
+        order_item.save
+      end
+      redirect_to admin_order_path(@order.id)
+    else
       redirect_to admin_order_path(@order.id)
     end
   end
